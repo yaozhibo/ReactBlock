1347596233048edd956c49a997d938e725c8cfb0
姚志博 <yyaozhibo@163.com>
2019-03-29 20:00:28 +0800

    wechat share

--- src/pages/article.js
+++ src/pages/article.js
@@ -46,7 +46,7 @@ class article extends PureComponent {
       payload: {},
     })
     const { articles: { wechatjsconfig } } = this.props
-    console.log(wechatjsconfig)
+    // console.log(wechatjsconfig)
     window.wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: 'wx68baa53e9882bd66', // 必填，公众号的唯一标识
@@ -60,14 +60,20 @@ class article extends PureComponent {
       title: 'test'
     }
     window.wx.ready(function () {
-      window.wx.onMenuShareTimeline({
-        title: window.globalShareDataWx.title
+      window.wx.updateTimelineShareData({
+        title: 'test', // 分享标题
+        link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: '', // 分享图标
+        success: function () {
+          // 设置成功
+          console.info('share success')
+        }
       })
     })
   }
 
   render() {
-    const { articles: { article, wechatjsconfig } } = this.props
+    const { articles: { article } } = this.props
 
     if (!article) {
       return (
