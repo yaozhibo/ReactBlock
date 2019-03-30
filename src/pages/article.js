6b4492c96714b198d4d4afc662f34c2a57bc1159
姚志博 <yyaozhibo@163.com>
2019-03-30 22:08:56 +0800

    wechat share

--- src/pages/article.js
+++ src/pages/article.js
@@ -47,6 +47,7 @@ class article extends PureComponent {
       callback: function (wechatjsconfig) {
         console.log(wechatjsconfig)
         if (wechatjsconfig) {
+          alert('123')
           window.wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx68baa53e9882bd66', // 必填，公众号的唯一标识
@@ -55,10 +56,14 @@ class article extends PureComponent {
             signature: wechatjsconfig.signature,// 必填，签名
             jsApiList: wechatjsconfig.jsApiList // 必填，需要使用的JS接口列表
           })
+
+          window.globalShareDataWx = {
+            title: 'test'
+          }
           window.wx.ready(function () {
             window.wx.updateTimelineShareData({
               title: 'test', // 分享标题
-              link: wechatjsconfig.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
               imgUrl: '', // 分享图标
               success: function () {
                 // 设置成功
@@ -66,18 +71,11 @@ class article extends PureComponent {
                 alert('share success')
               }
             })
-            window.wx.checkJsApi({
-              jsApiList: wechatjsconfig.jsApiList, // 需要检测的JS接口列表，所有JS接口列表见附录2,
-              success: function (res) {
-                // 以键值对的形式返回，可用的api值true，不可用为false
-                // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-              }
-            });
             alert('share')
           })
           window.wx.error(function (res) {
             // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-            alert('Error' + res)
+            alert(res)
           });
         }
 
