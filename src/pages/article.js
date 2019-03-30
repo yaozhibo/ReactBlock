6ab5edba4419578592081dd087cac0bc08752794
姚志博 <yyaozhibo@163.com>
2019-03-30 23:07:20 +0800

    wechat share

--- src/pages/article.js
+++ src/pages/article.js
@@ -51,9 +51,9 @@ class article extends PureComponent {
           window.wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx68baa53e9882bd66', // 必填，公众号的唯一标识
-            timestamp: '1553956339', // 必填，生成签名的时间戳
-            nonceStr: 'cLgcByCaKinxR2RR', // 必填，生成签名的随机串
-            signature: '67888d5c263d269427627b1751fac650650daf3a',// 必填，签名
+            timestamp: `"${wechatjsconfig.timestamp}"`, // 必填，生成签名的时间戳
+            nonceStr: wechatjsconfig.nonceStr, // 必填，生成签名的随机串
+            signature: wechatjsconfig.signature,// 必填，签名
             jsApiList: wechatjsconfig.jsApiList // 必填，需要使用的JS接口列表
           })
 
